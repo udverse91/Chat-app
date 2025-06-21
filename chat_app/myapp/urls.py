402c78314039fff3e270a1aca0aec53e33aabc34
udverse91 <agrawaluday14@gmail.com>
2025-06-21 15:02:31 +0530

    Delete chat_app directory

--- chat_app/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # Home Page
-    path('about/', views.about, name='about'),  # About Page
-    path('chat/', views.CreateRoom, name='chat-room'),  # Chat Form Page (formerly index.html)
-    path('<str:room_name>/<str:username>/', views.MessageView, name='room'),  # Chat Messages Page
-]
